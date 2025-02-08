3059392d89fa61a6fb7f4fcf1aa79316453b8c2a
Javier Castilla <jcastp@pm.me>
2025-02-08 11:04:17 +0100

    Aporetic fonts installed

--- install_fonts.sh
+++ install_fonts.sh
@@ -5,6 +5,10 @@
 echo "Installing Iosevka-Comfy fonts"
 (git clone --depth 1 https://github.com/protesilaos/iosevka-comfy.git /home/tmp/iosevka-comfy || (cd /home/tmp/iosevka-comfy/ && git pull)) && (cp /home/tmp/iosevka-comfy/*/TTF/*.ttf ~/.local/share/fonts/ && fc-cache -f)
 
+# Install the aporetic fonts (https://protesilaos.com/codelog/2025-02-04-aporetic-fonts-1-0-0/)
+echo "Installing Aporetic fonts"
+(git clone --depth 1 https://github.com/protesilaos/aporetic.git /home/tmp/aporetic-fonts || (cd /home/tmp/aporetic-fonts/ && git pull)) && (cp /home/tmp/aporetic-fonts/*/TTF/*.ttf ~/.local/share/fonts/ && fc-cache -f)
+
 # Install the lexend fonts
 echo "Installing Lexend fonts"
 # (git clone https://github.com/googlefonts/lexend /home/tmp/lexend/ || (cd /home/tmp/lexend/ && git pull)) && font-manager -i /home/tmp/lexend/fonts/lexend/ttf/*.ttf
