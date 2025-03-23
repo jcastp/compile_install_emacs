342f9c4a0eecdddda72bef783c220ecb04d46ad3
Javier Castilla <jcastp@pm.me>
2025-03-23 12:17:28 +0100

    Reorganized the fonts sections

--- install_fonts.sh
+++ install_fonts.sh
@@ -4,10 +4,19 @@
 echo "Installing Aporetic fonts"
 (git clone --depth 1 https://github.com/protesilaos/aporetic.git /home/tmp/aporetic-fonts || (cd /home/tmp/aporetic-fonts/ && git pull)) && (cp /home/tmp/aporetic-fonts/*/TTF/*.ttf ~/.local/share/fonts/ && fc-cache -f)
 
-# Install the lexend fonts
-echo "Installing Lexend fonts"
-# (git clone https://github.com/googlefonts/lexend /home/tmp/lexend/ || (cd /home/tmp/lexend/ && git pull)) && font-manager -i /home/tmp/lexend/fonts/lexend/ttf/*.ttf
-(git clone https://github.com/googlefonts/lexend /home/tmp/lexend/ || (cd /home/tmp/lexend/ && git pull)) && (cp /home/tmp/lexend/fonts/lexend/ttf/*.ttf ~/.local/share/fonts/ && fc-cache -f)
-echo "Installing Lexend Italic fonts"
-# (git clone https://github.com/richardhriech/LexendItalic /home/tmp/lexend_italic/ || (cd /home/tmp/lexend_italic/ && git pull)) && font-manager -i /home/tmp/lexend_italic/*.ttf
-(git clone https://github.com/richardhriech/LexendItalic /home/tmp/lexend_italic/ || (cd /home/tmp/lexend_italic/ && git pull)) && (cp /home/tmp/lexend_italic/*.ttf ~/.local/share/fonts/ && fc-cache -f)
+# install fonts
+sudo apt install -y font-manager
+sudo apt install -y fonts-firacode
+
+echo "checking ETBembo font."
+font-manager -l | grep "ETBembo" >/dev/null
+if [[ $? -eq 0 ]]; then
+  # If the pattern is found, execute this block
+  echo "ETBembo font installed."
+  # Add your commands or actions here
+else
+  # If the pattern is not found, execute this block
+  echo "Installing ETBembo font."
+  font-manager -i ~/Nextcloud/config/fonts/ET_Bembo/*.otf
+  # Add your commands or actions here
+fi
