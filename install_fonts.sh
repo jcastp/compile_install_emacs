8bd57559fac36676c046693aaa223a478ab5cbd0
Javier Castilla <jcastp@pm.me>
2026-02-24 17:46:15 +0100

    Fixed some problematic code

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env sh
+set -e
+
+INSTALL_DIR=/home/tmp
+ETBEMBO_DIR="${ETBEMBO_DIR:-$HOME/Nextcloud/config/fonts/ET_Bembo}"
 
 # Install the aporetic fonts (https://protesilaos.com/codelog/2025-02-04-aporetic-fonts-1-0-0/)
 echo "Installing Aporetic fonts"
-(git clone --depth 1 https://github.com/protesilaos/aporetic.git /home/tmp/aporetic-fonts || (cd /home/tmp/aporetic-fonts/ && git pull)) && (cp /home/tmp/aporetic-fonts/*/TTF/*.ttf ~/.local/share/fonts/ && fc-cache -f)
+(git clone --depth 1 https://github.com/protesilaos/aporetic.git "$INSTALL_DIR/aporetic-fonts" || (cd "$INSTALL_DIR/aporetic-fonts/" && git pull)) && (cp "$INSTALL_DIR/aporetic-fonts"/*/TTF/*.ttf ~/.local/share/fonts/ && fc-cache -f)
 
 # install fonts
 sudo apt install -y font-manager
 sudo apt install -y fonts-firacode
 
-echo "checking ETBembo font."
-font-manager -l | grep "ETBembo" >/dev/null
-if [[ $? -eq 0 ]]; then
-  # If the pattern is found, execute this block
-  echo "ETBembo font installed."
-  # Add your commands or actions here
+echo "Checking ETBembo font."
+if fc-list | grep -qi "ETBembo"; then
+  echo "ETBembo font already installed."
 else
-  # If the pattern is not found, execute this block
   echo "Installing ETBembo font."
-  font-manager -i ~/Nextcloud/config/fonts/ET_Bembo/*.otf
-  # Add your commands or actions here
+  if [[ -d "$ETBEMBO_DIR" ]]; then
+    cp "$ETBEMBO_DIR"/*.otf ~/.local/share/fonts/
+    fc-cache -f
+  else
+    echo "ETBembo source directory not found: $ETBEMBO_DIR"
+  fi
 fi
